aee0a2e1a771bb03a6ac5ce1ae1ce7ad37235880
SamuelLopez7 <49240225+SamuelLopez7@users.noreply.github.com>
2019-04-04 18:40:33 -0400

    actualizado

--- src/sistema_postre/listaPostre.java
+++ src/sistema_postre/listaPostre.java
@@ -1,5 +1,39 @@
 package sistema_postre;
 
-public class listaPostre {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class listaPostre implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	
+	
+	{
+		
+		try {
+			
+			Connection miConexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba","root","");
+			Statement miStatement=miConexion.createStatement();
+			ResultSet miResultado=miStatement.executeQuery("SELECT * FROM postres");
+			
+			while(miResultado.next()) 
+			{
+				
+				System.out.println(miResultado.getString("nombre"));
+			}
+			
+		}catch(Exception ee)
+		{
+			
+			System.out.println("no conecta");
+			
+		}
+	}
+	
+	
+	
+	
 
 }
